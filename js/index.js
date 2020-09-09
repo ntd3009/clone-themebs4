781a9819d4f158c58ad0191527e2b04e33e2999b
ntd3009 <ntd.300996@gmail.com>
2020-09-09 15:33:30 +0700

    features section

--- js/index.js
+++ js/index.js
@@ -1,43 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
   const $ = jQuery(this);
+  const header = document.getElementById('header');
   const mediaQMWidth991 = window.matchMedia("(max-width: 991px)");
 
   const btnShowHide = document.getElementById('js-show-hide-navbar');
   const navbar = document.getElementById('navbar-header');
 
-  
-  function navbarAction() {
-    btnShowHide.addEventListener('click', function() {
-      navbar.classList.toggle('open');
-    });
-    
-    window.addEventListener('resize', function() {
-      if(navbar.classList.contains('open')) {
-        navbar.classList.remove('open');
-      }
-    });
-  };
-
   let megaDropLv1 = $.find('.mega-drop-lv1');
   let megaDropLv2 = $.find('.mega-drop-lv2');
 
-  let removeAllActive = function(items ) {
-    items.each( function(i) {
+  let openNavbar = function () {
+    btnShowHide.addEventListener('click', function () {
+      navbar.classList.toggle('open');
+    });
+  };
+
+  let removeAllActive = function (items) {
+    items.each(function (i) {
       let hasClass = this.classList.contains('active');
-      if(hasClass) {
+      if (hasClass) {
         this.classList.remove('active');
       }
     });
   }
 
-  let eventAc = function(dom) {
+  let eventAc = function (dom) {
     let heads = dom.children('.btn-drop-menu');
-  
-    heads.each( function(i) {
+    heads.each(function (i) {
       this.addEventListener('click', () => {
         let wrap = this.parentNode;
         let currentHasAc = wrap.classList.contains('active');
-        if(!currentHasAc) {
+        if (!currentHasAc) {
           removeAllActive(dom);
           removeAllActive(megaDropLv2);
           wrap.classList.add('active');
@@ -49,26 +42,30 @@ $(document).ready(function(){
     });
   }
 
-  let checkMedia = function(mq) {
+  let megaMenuRps = function (mq) {
     if (mq.matches) {
+      openNavbar();
       eventAc(megaDropLv1);
       eventAc(megaDropLv2);
     }
+    window.addEventListener('resize', function () {
+      megaMenuRps(mq);
+      if (navbar.classList.contains('open')) {
+        navbar.classList.remove('open');
+      }
+    });
   }
-  checkMedia(mediaQMWidth991);
-  navbarAction();
-  
+
+  megaMenuRps(mediaQMWidth991);
+
   $.find('.hero [data-fancybox]').fancybox({
     youtube: {
-      controls : 1,
-      showinfo : 0
+      controls: 1,
+      showinfo: 0
     },
     vimeo: {
-      color : 'f00'
+      color: 'f00'
     }
   });
 
 });
-
-
-
